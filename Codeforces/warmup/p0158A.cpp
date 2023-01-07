e8003e65d475ddcb382325a6370b82019d12d7d0
Robin Hafid Quintero Lopez <rohaquinlop301@gmail.com>
2023-01-06 23:03:20 -0500

    06-01-2023
    Training - 800 pts.
    With Kaner as my partner.

---
+++ Codeforces/warmup/p0158A.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+typedef vector <int> vi;
+typedef vector <ll> vll;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+
+typedef vector < pii > vpii;
+typedef vector < pll > vpll;
+
+typedef vector <string> vs;
+typedef vector < vi > vvi;
+typedef vector < vll > vvll;
+
+#define fl(i, a, b)	for (int i = a; i < b; ++i)
+
+#define all(v) 	(v).begin(), (v).end()
+#define srt(v)	sort (all (v))
+
+#define pb push_back
+#define mp make_pair
+
+#define dig(i)	(s[i] - '0')
+#define slen(s)	s.length()
+
+#define fr first
+#define sc second
+
+#define len(x) x.size()
+#define fill(x, y)	memset(x, y, sizeof(x))
+#define clr(a)	fill(a, 0)
+#define endl '\n'
+
+#define PI 3.14159265358979323
+
+#define trace1(x1)					cerr << #x1 << ": " << x1 << endl;
+#define trace2(x1, x2)				cerr << #x1 << ": " << x1 << " | " << #x2 << ": " << x2 << endl;
+#define trace3(x1, x2, x3)			cerr << #x1 << ": " << x1 << " | " << #x2 << ": " << x2 << " | " << #x3 << ": " << x3 << endl;
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+
+const ll MOD = 1000000007LL;
+const ll MAX = 100010LL;
+
+template <typename T> T gcd(T a, T b) { if (b == 0) return a; return gcd(b, a % b);}
+template <typename T> T power(T x, T y, ll m = MOD) { T ans = 1; x %= m; while (y > 0) { if (y & 1ll) ans = (ans * x) % m; y >>= 1ll; x = (x * x)%m; } return ans%m;}
+
+int solve(vi& v, int k) {
+  int pos = -1;
+
+  for(int i = v.size()-1; i > -1 and pos == -1; i--) {
+    if(v[i] >= v[k] and v[i]) pos = i;
+  }
+
+  if(pos==-1) return 0;
+  else return pos+1;
+}
+
+int main() {
+	FAST_IO;
+  int n, k;
+
+  cin >> n >> k;
+
+  vi v(n);
+
+  for(int &x : v) cin >> x;
+
+  cout << solve(v, k-1) << endl;
+
+
+	return 0;
+}
